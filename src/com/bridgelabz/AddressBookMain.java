79a0b22798590c0c36bb9eeeffcebdcb08682447
SUDIPP22 <panja370@gmail.com>
2021-08-12 19:16:34 +0530

    Added : Welcome Message

---
+++ src/com/bridgelabz/AddressBookMain.java
@@ -0,0 +1,17 @@
+package com.bridgelabz;
+
+/**
+ * Purpose : To Create an Address Book
+ * @author SUDIP PANJA
+ * @version 11.0.11
+ * @since 2021-08-12
+ */
+public class AddressBookMain {
+    /**
+     * This main method is used to print welcome message
+     * @param args Unused
+     */
+    public static void main(String[] args) {
+        System.out.println("Welcome To Address Book Program");
+    }
+}
