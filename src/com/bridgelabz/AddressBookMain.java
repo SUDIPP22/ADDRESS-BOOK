b8f04e19a7f0391a641e21c5214aaf939e00cbd9
SUDIPP22 <panja370@gmail.com>
2021-08-13 14:54:05 +0530

    Refactored : Creating a contact in Address Book

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -1,17 +1,28 @@
 package com.bridgelabz;
 
-/**
- * Purpose : To Create an Address Book
- * @author SUDIP PANJA
- * @version 11.0.11
- * @since 2021-08-12
- */
 public class AddressBookMain {
     /**
-     * This main method is used to print welcome message
+     * This main method is used to print welcome message and
+     * displaying a person's detail
      * @param args Unused
      */
     public static void main(String[] args) {
         System.out.println("Welcome To Address Book Program");
+
+        Contacts contact = new Contacts("Sudip ", "Panja ", "1/A/7 ", "Kolkata ",
+                "West Bengal", 700050L, 8910211371L, "panja370@gmail.com ");
+        System.out.println("Name : " +contact.getFirstName()+ " " +contact.getLastName());
+        System.out.println("Address : " +contact.getAddress());
+        System.out.println("City : " +contact.getCity());
+        System.out.println("State : " +contact.getState());
+        System.out.println("Zip Code : " +contact.getZip());
+        System.out.println("Phone Number : " +contact.getPhoneNumber());
+        System.out.println("E-Mail Id : " +contact.getEmail());
+
     }
 }
+
+
+
+
+
